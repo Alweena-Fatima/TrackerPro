cc9962f2ef8611549a7f3fcb308f55a835e334c0
Alweena-Fatima <alweenacse@gmail.com>
2025-09-03 02:43:13 +0530

    k

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,4 @@
 import app from "./app.js";
 import serverless from "serverless-http";
 
-// Wrap your Express app with serverless-http
-export const handler = serverless(app);
+export default serverless(app); // ✅ default export
